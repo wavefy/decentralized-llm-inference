aa489c73364ecdf97ba20dd25701cbd3825c7236
shadowWalker <hieu811@gmail.com>
2024-09-19 00:41:35 +0700

    feat: desktop app (#2)
    * add tauri for desktop app
    
    * add simple desktop app
    
    * add openai-react-chat for view
    
    * fix no need to filter models
    
    * use open-ai server in tauri
    
    * add execute dev for app

---
+++ bin/desktop-app/run-app.sh
@@ -0,0 +1,4 @@
+NODE_ID=1 \
+LAYERS_FROM=0 \
+LAYERS_TO=12 \
+cargo tauri dev
---
+++ bin/desktop-app/view/src/constants/apiEndpoints.ts
@@ -0,0 +1,4 @@
+export const OPENAI_ENDPOINT = 'http://localhost:1234';
+export const TTS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/audio/speech`;
+export const CHAT_COMPLETIONS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/chat/completions`;
+export const MODELS_ENDPOINT = `${OPENAI_ENDPOINT}/v1/models`;
