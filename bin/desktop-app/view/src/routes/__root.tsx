98a5e4cf8e3ca309d0bce7ab54853cb682b8262b
Luong Minh <ngocminhluong182@gmail.com>
2024-10-14 08:37:38 +0700

    feat: new health page

--- bin/desktop-app/view/src/routes/__root.tsx
+++ bin/desktop-app/view/src/routes/__root.tsx
@@ -1,34 +1,53 @@
-import { Toaster } from '@/components/ui/sonner'
-import { createRootRoute, Link, Outlet } from '@tanstack/react-router'
-import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import { Toaster } from "@/components/ui/sonner";
+import {
+  createRootRoute,
+  Link,
+  Outlet,
+  useMatchRoute,
+} from "@tanstack/react-router";
+import { TanStackRouterDevtools } from "@tanstack/router-devtools";
 
 export const Route = createRootRoute({
-  component: () => (
+  component: () => <RootComponent />,
+});
+
+const RootComponent = () => {
+  const hideNavRoutes = ["/health"];
+
+  const matchRoute = useMatchRoute();
+
+  const matchedHideNavRoutes = hideNavRoutes.some((route) =>
+    matchRoute({ to: route })
+  );
+
+  return (
     <div className="flex flex-col h-screen">
-      <nav className="bg-accent text-white p-4">
-        <div className="flex justify-between items-center">
-          <div className="text-xl font-bold">Wavefy DLLM Demo</div>
-          <div className="space-x-4">
-            <Link
-              to="/"
-              className="hover:text-gray-300 transition-colors duration-200 [&.active]:text-blue-400 [&.active]:font-semibold"
-            >
-              Chat
-            </Link>
-            <Link
-              to="/dashboard"
-              className="hover:text-gray-300 transition-colors duration-200 [&.active]:text-blue-400 [&.active]:font-semibold"
-            >
-              Dashboard
-            </Link>
+      {!matchedHideNavRoutes && (
+        <nav className="bg-accent text-white p-4">
+          <div className="flex justify-between items-center">
+            <div className="text-xl font-bold">Wavefy DLLM Demo</div>
+            <div className="space-x-4">
+              <Link
+                to="/"
+                className="hover:text-gray-300 transition-colors duration-200 [&.active]:text-blue-400 [&.active]:font-semibold"
+              >
+                Chat
+              </Link>
+              <Link
+                to="/dashboard"
+                className="hover:text-gray-300 transition-colors duration-200 [&.active]:text-blue-400 [&.active]:font-semibold"
+              >
+                Dashboard
+              </Link>
+            </div>
           </div>
-        </div>
-      </nav>
+        </nav>
+      )}
       <div className="flex-1">
         <Outlet />
       </div>
       <Toaster />
       {/* <TanStackRouterDevtools /> */}
     </div>
-  ),
-})
+  );
+};
